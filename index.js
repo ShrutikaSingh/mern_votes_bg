3d5c47f6efd36bdedd5d90aa1de09a3f48e4da75
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-24 12:26:56 +0530

    correct the route for /api/register

--- index.js
+++ index.js
@@ -18,13 +18,28 @@ app.use(cors());    //initialise the middle wares that we installed
 app.use(bodyParser.json()); //initialise the middle wares body-parser that we installed
 
 app.get('/',(req,res)=>res.json ({hello :'worlds'}));//json object //start listing at /
-app.use('./api/auth',routes.auth);//sending all the route for this endpoint here and routes.auth will take care of all the auth
-//here mkdir routes
-//touch routes/index.js & touch routes/auth.js
+app.use('/api/auth',routes.auth);//sending all the route for this endpoint here and routes.auth will take care of all the auth
+
+
 
 
 
 app.use(handle.notFound);   //not found is coming from handler/index.js
-//app.use( handle.errors);    //this errorHandler errors is coming from handler/index.js//const port= process.env.PORT; //it will take the PORT variable from .env
 
+//app.use( handle.errors);    //this errorHandler errors is coming from handler/index.js//const port= process.env.PORT; //it will take the PORT variable from .env
+//getting error res.status is not a function
 app.listen(port, console.log(`server is listining to ${port}`)); //``backtick for template literals
+
+
+
+
+
+
+
+
+
+
+
+
+
+
