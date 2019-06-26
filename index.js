0a73e23b778f73eb6a136a05d1cc64032895ab03
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-26 20:20:36 +0530

    udated the PORT and env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();//create private variables that u dont want to share on a website like github
-//create server
+
 
 const express = require('express');
 //const port= 5000;   //
@@ -19,17 +19,20 @@ app.use(cors());    //initialise the middle wares that we installed
 app.use(bodyParser.json()); //initialise the middle wares body-parser that we installed
 
 app.get('/',(req,res)=>res.json ({hello :'worlds'}));//json object //start listing at /
+
 app.use('/api/auth',routes.auth);//sending all the route for this endpoint here and routes.auth will take care of all the auth
-
-
-
-
+//go to postman to see the users  and post {"username":"ff","password":"ydfuesfrdttyefa"} to localhost:port_number/api/auth/login in the body
+//and localhost:5000/api/auth/register to register a new user  using new {"username":"tufdyhuetrtrehi","password":"ydfuesrttyefa"} and post
 
 app.use(handle.notFound);   //not found is coming from handler/index.js
 
 //app.use( handle.errors);    //this errorHandler errors is coming from handler/index.js//const port= process.env.PORT; //it will take the PORT variable from .env
 //getting error res.status is not a function
-app.listen(process.env.PORT || 5000 , console.log(`server is listining to ${port}`)); //``backtick for template literals
+//app.listen(process.env.PORT || 5000 , console.log(`server is listining to ${port}`)); //``backtick for template literals
+
+
+
+app.listen(process.env.PORT,console.log(`server is ready`));
 
 
 
