701210e09d5168ccbfbc819a857d572b060ad400
ShrutikaSingh <shrutika051220@gmail.com>
2019-07-11 11:00:47 +0530

    add post method for createPolls

--- routes/poll.js
+++ routes/poll.js
@@ -3,7 +3,10 @@ const router=require('express').Router();
 
 //so what all the routers do we need
 //at / route we will display all the polls
-router.route('/').get()//show every thing
 
+//router.route('/').get()//show every thing,initially it was this to show everything but now we have to crate a showpoll method in handlers/poll.js
+const handle=require('../handlers');
+router.route('/').get(handle.showPolls)
+.post(handle.createPolls);
 
 module.exports = router //exportinng router
