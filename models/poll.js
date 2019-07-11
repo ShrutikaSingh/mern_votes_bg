04014ebe47bc55c02474fac4c2058829d3328c64
ShrutikaSingh <shrutika051220@gmail.com>
2019-07-11 15:05:06 +0530

    correct spelling of questions to question to be used by the userPoll method of handlers/poll.js

--- models/poll.js
+++ models/poll.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 
 
 const optionSchema = new mongoose.Schema({
-    options:String,
+    option:String,
     votes:{
         type:Number,
         default:0,
@@ -14,9 +14,9 @@ const pollSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
-    questions:String,
+    question:String,
     options:[optionSchema], //to refer optionSchema above
-    voted:[{type: mongoose.Schema.Types.ObjectId,ref:'User'}],
+    voted:[{type: mongoose.Schema.Types.ObjectId,ref:'User'}], //The ref option is what tells Mongoose which model to use during population,
     created:{
         type:Date,
         default:Date.now,
