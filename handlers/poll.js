91bfe2ead1b8027930be580b2465961db709480d
ShrutikaSingh <shrutika051220@gmail.com>
2019-07-11 10:58:53 +0530

    add create poll for service and option getting TypeError of map undefined

--- handlers/poll.js
+++ handlers/poll.js
@@ -9,10 +9,30 @@ exports.showPolls=async(req,res,next) => {
     try{
         const polls= await db.Poll.find(); //find everything
         res.status(200).json(polls)//200 for OK status and show the polls in json format
-
     }
     catch(err){
         err.status= 400;
         next(err);
     }
 };
+
+exports.createPolls=async (req,res,next)=>{
+    try{
+        const {service, options} = req.body; //destructring req.body 
+        const poll= await db.Poll.create({
+        service,
+        options: options.map(option => ({ option, //ERROR IN HERE
+            // TypeError: Cannot read property 'map' of undefined
+            votes: 0
+        }))  
+        });
+        
+        res.status(201).json(poll)//201 is status for created
+        
+    }
+    catch(err)
+    {
+        err.status=400;
+        next(err);
+    }
+}
