eeea6065cea5e899c7f2b6bdc66eb0cb9cd15443
ShrutikaSingh <shrutika051220@gmail.com>
2019-07-27 23:11:54 +0530

    update hanlders by deleting handlers of appointment form

--- handlers/index.js
+++ handlers/index.js
@@ -2,14 +2,15 @@
 //#002
 module.exports = {
     ...require('./auth'), //... spread operater so as to spread out every individual handler inside of auth {refers to the file handlers/auth.js}
-    ...require('./poll')//for importing poll.js from handlers/poll.js
+    ...require('./poll'),//for importing poll.js from handlers/poll.js
+    
 };
 
 
 
 //next is ver imp to errors
 module.exports.notFound=(res,req,next)=>{
- const err=new Error('not found');
+ const err=new Error('not found here i m');
  err.status=404;
  next(err);
 };
